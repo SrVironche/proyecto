e6cd09570fd4aa5979a2ae1e6c5e1c1e8d8888a3
SrVironche <alejandrovironche@gmail.com>
2025-03-05 23:00:36 -0700

    listo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import motoLogo from './descarga.jpg'; 
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [moto, setMoto] = useState(""); 
+  const [motosGuardadas, setMotosGuardadas] = useState([]);
+
+  const handleInputChange = (e) => {
+    setMoto(e.target.value);
+  };
+
+  const handleSaveMoto = () => {
+    if (moto) {
+      setMotosGuardadas([...motosGuardadas, moto]);
+      setMoto("");
+    }
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <componentes />
-      </header>
+        <img src={motoLogo} className="App-logo" alt="Moto Logo" />
+        <h1>¿Que moto te gustaria?</h1>
+        <p>escribe la lista de motos que te gustaria tener.</p>
+        
+        <div>
+          <input 
+            type="text" 
+            placeholder="Escribe el nombre de una moto..." 
+            value={moto} 
+            onChange={handleInputChange} 
+          />
+          <button onClick={handleSaveMoto}>Guardar</button>
+        </div>
+
+        <div>
+          <h3>Motos guardadas:</h3>
+          <ul>
+            {motosGuardadas.map((moto, index) => (
+              <li key={index}>{moto}</li> 
+            ))}
+          </ul>
+        </div>
+      </header> 
     </div>
   );
 }
